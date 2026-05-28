583d275f70fabe2accbb70c3e8fef98b3a912a69
Nidhi Saxena <emailnidhisa26@gmail.com>
2026-05-28 17:50:19 +0530

    app.py

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
-
 from flask import Flask, request, jsonify
 import numpy as np
 import tensorflow as tf
+import os
 
 app = Flask(__name__)
 
+# Load model
 model = tf.keras.models.load_model("model/model.keras")
 
 @app.route("/")
 def home():
-    return "PPG API Running"
+    return "PPG API Running Successfully"
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    data = request.json["signal"]
-    data = np.array(data).reshape(1, 4000, 1)
+    try:
+        data = request.json["signal"]
+        data = np.array(data).reshape(1, 4000, 1)
 
-    pred = model.predict(data)[0][0]
+        pred = model.predict(data)[0][0]
 
-    return jsonify({"prediction": float(pred)})
+        return jsonify({
+            "prediction": float(pred)
+        })
 
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        })
+
+# IMPORTANT: Render port fix
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
